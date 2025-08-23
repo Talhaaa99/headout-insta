e262cacfb36f0993b2e84673adec65b814f3064b
Talhaaa99 <talha.toufeeq99@gmail.com>
2025-08-24 01:20:46 +0530

    build: :bug: deployment fix

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -7,6 +7,7 @@ import sharp from "sharp";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+export const maxDuration = 30;
 
 export async function POST(req: NextRequest) {
   try {
